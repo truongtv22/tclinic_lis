ad92b32209e54103420db63b405370eaa02b3781
Tạ Trường <truongtv22@gmail.com>
2024-02-20 16:30:24 +0700

    Login page

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -2,20 +2,36 @@ import { Link } from 'react-router-dom';
 import { Form, Input, Button } from 'antd';
 
 export function LoginPage() {
+  const onSubmit = (values) => {
+    console.log('onSubmit', values);
+  };
+
   return (
     <div className="h-full flex items-center justify-center">
-      <div className="w-96 p-6 shadow-lg rounded">
-        <div className="flex items-center space-x-2">
+      <div className="w-96 p-6 space-y-8 rounded shadow-[0_0_100px_rgba(0,0,0,0.08)]">
+        <div className="flex items-center justify-center space-x-2">
           <div className="w-8 h-8 bg-slate-400 rounded-lg" />
           <h2 className="text-xl">tClinic LIS</h2>
         </div>
-        <div>
-          <Form>
-            <Form.Item name="taikhoan">
-              <Input placeholder="Tài khoản" />
-            </Form.Item>
-          </Form>
-        </div>
+        <Form onFinish={onSubmit}>
+          <Form.Item
+            name="taikhoan"
+            rules={[{ required: true, message: 'Không được để trống' }]}
+            hasFeedback
+          >
+            <Input placeholder="Tài khoản" />
+          </Form.Item>
+          <Form.Item
+            name="matkhau"
+            rules={[{ required: true, message: 'Không được để trống' }]}
+            hasFeedback
+          >
+            <Input.Password placeholder="Mật khẩu" />
+          </Form.Item>
+          <Button type="primary" htmlType="submit" className="w-full">
+            Đăng nhập
+          </Button>
+        </Form>
       </div>
     </div>
   );
